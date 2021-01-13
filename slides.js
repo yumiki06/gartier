77a6da5b6037ad78bed6f54149769434bfeb3d53
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2021-01-13 22:40:52 +0900

    JS final

--- slides.js
+++ slides.js
@@ -1,23 +1,17 @@
-var slides = document.getElementsByClassName('slides')[0].getElementsByTagName('li');
-
-//スライド表示用の関数を呼び出す（引数はスライドの切り替え時間）
-viewSlide(3000);
-
-function viewSlide(msec, slide_no = -1)
+window.addEventListener('load', function () {
+	viewSlide('.slides img');
+});
+function viewSlide(className, slideNo = -1)
 {
-	//現在のスライドを消す
-	if (slides[slide_no]) {
-		slides[slide_no].style.display = 'none';
+	let imgArray = document.querySelectorAll(className);
+	if (slideNo >= 0) {
+		//初回以外は現在のスライドを消す
+		imgArray[slideNo].style.opacity = 0;
 	}
-	//スライド番号をカウントアップ
-	slide_no++;
-	if (slides[slide_no]) {
-		//次のスライドを表示
-		slides[slide_no].style.display = 'block';
-	} else {
-		//次のスライドがなければ最初のスライドを表示
-		slides[0].style.display = 'block';
-		slide_no = 0;
+	slideNo++;
+	if (slideNo >= imgArray.length) {
+		slideNo = 0; //次のスライドがなければ最初のスライドへ戻る
 	}
-	setTimeout(function(){viewSlide(msec, slide_no);}, msec);
+	imgArray[slideNo].style.opacity = 1;
+	setTimeout(function(){viewSlide(className, slideNo);}, 3000);
 }
